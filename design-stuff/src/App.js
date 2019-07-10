300bbc2d7c6c95637b2b81e8d86d0e23566892ef
Alexander <maniacallion@gmail.com>
2019-07-09 23:19:01 -0500

    theres a basic table, i can't do fancy right now

--- design-stuff/src/App.js
+++ design-stuff/src/App.js
@@ -3,6 +3,7 @@ import './styles/core/base.scss';
 
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
+import Table from './components/table/table';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <section>
         some tidly bits we be here once i knonw what i wantn to do with this
       </section>
+      <Table />
       <Footer />
     </div>
   );
--- design-stuff/src/components/header/header.js
+++ design-stuff/src/components/header/header.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import('../../styles/core/header.scss');
 
+//TODO: finish drop down
 function Header() {
   return (
+    <>
     <header>
-        <nav>
-          <ul>
-            <li>Page 1</li>
-            <li>Page 2</li>
-            <li>Page 3</li>
-            <li>Page 4</li>
-          </ul>
-        </nav>
+      <nav className="navbar">
+        <ul>
+          <li>page 1</li>
+          <li>page 2</li>
+        </ul> 
+        <div class="dropdown">
+            <ul className='dropdown'>
+              <li>Link 1</li>
+              <li>Link 2</li>
+              <li>Link 3</li>
+            </ul>
+        </div> 
+      </nav>
+      <h2>Design Playground... ain't she purdy </h2>
     </header>
+    </>
   );
 }
 
